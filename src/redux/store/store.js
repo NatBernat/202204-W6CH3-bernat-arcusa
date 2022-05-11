eb0b92c02805f18aec3a64254c4a5a382cc89475
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-11 21:07:01 +0200

    Added redux dataflow for addDigit action

---
+++ src/redux/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import phoneReducer from "../features/phoneSlice/phoneSlice";
+
+export const store = configureStore({
+  reducer: {
+    phone: phoneReducer,
+  },
+});
